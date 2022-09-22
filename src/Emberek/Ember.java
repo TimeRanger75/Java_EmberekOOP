72fd5e134f73160915aa2afe6b7f59d7d21930ce
Márk Moravcsik <Moravcsikmark@gmail.com>
2022-09-22 15:32:28 +0200

    Initial commit

---
+++ src/Emberek/Ember.java
@@ -0,0 +1,14 @@
+package Emberek;
+
+public class Ember {
+
+    private String nev;
+    private String szulDatum;
+    private String szulHely;
+
+    public Ember(String nev, String szulDatum , String szulHely){
+        this.nev=nev;
+        this.szulDatum=szulDatum;
+        this.szulHely=szulHely;
+    }
+}
