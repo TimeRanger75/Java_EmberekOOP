5caea2202bc03bf9d1e012aae9232d8246ca4b4f
Várszegi Ádám <varszegi.adam@petrik.hu>
2022-09-22 16:03:53 +0200

    3 különböző objektum Születési év, hónap, nap metódus

--- src/Emberek/Ember.java
+++ src/Emberek/Ember.java
@@ -13,7 +13,24 @@ public class Ember {
     }
 
 
+    public int SzulEv(){
+        int szulEv=Integer.parseInt(szulDatum.substring(0,4));
+        return  szulEv;
+    }
+
+    public int SzulHo(){
+        int szulHo=Integer.parseInt(szulDatum.substring(5,7));
+        return  szulHo;
+    }
+
+    public int SzulNap(){
+        int szulNap=Integer.parseInt(szulDatum.substring(8,10));
+        return  szulNap;
+    }
+
+
+
     @Override
-    public String toString() {return String.format("%s született %s , %s településen",this.nev,this.szulDatum,this.szulHely);}
+    public String toString() {return String.format("%30s  %10s %-20s ",this.nev,this.szulDatum,this.szulHely);}
 
 }
--- src/Emberek/Main.java
+++ src/Emberek/Main.java
@@ -4,6 +4,14 @@ import  Emberek.Ember;
 public class Main {
     public static void main(String[] args) {
         Ember e1=new Ember("Alfonzo","1987-11-22","Miskolc");
+        Ember e2=new Ember("Adam Jensen","1987-04-06","Detroit");
+        Ember e3=new Ember("Példa Béla","2000-07-30","Szeged");
         System.out.println(e1);
+        System.out.println(e2);
+        System.out.println(e3);
+
+        System.out.println(e1.SzulEv());
+
+        System.out.printf("%s, %s, %s",e1.SzulEv(),e2.SzulEv(),e3.SzulEv());
     }
 }
